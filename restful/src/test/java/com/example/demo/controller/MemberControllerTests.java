5491c7cd569bb92d265bea4c3df389daeffb3b63
spiderman <spiderman@xhift.co.kr>
2025-07-17 14:59:41 +0900

    added some sample for andExpect

--- restful/src/test/java/com/example/demo/controller/MemberControllerTests.java
+++ restful/src/test/java/com/example/demo/controller/MemberControllerTests.java
@@ -2,19 +2,17 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.MemberRequest;
 import com.example.demo.dto.MemberResponse;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.json.JsonCompareMode;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -39,16 +37,23 @@ public class MemberControllerTests {
 
         // prepare json string from memberRequest
         String requestString = objectMapper.writeValueAsString(memberRequest);
+        String requestStringTemp = "{ \"name\": \"윤서준\", \"email\": \"SeojunYoon@hanbit.co.kr\", \"age\": 10 }";
 
         // prepare request builder
         RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/api/members")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaType.APPLICATION_JSON_VALUE)
-                .content(requestString);
+                .content(requestStringTemp);
 
         // request to MockMvc and validate status code
         MvcResult mvcResult = mockMvc.perform(requestBuilder)
                 .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(header().string("Content-Type", "application/json"))
+                .andExpect(content().json("{ name: '윤서준' }", JsonCompareMode.LENIENT))
+                .andExpect(content().json("{ name: 윤서준 }", JsonCompareMode.LENIENT))
+                .andExpect(content().json("{ \"name\": \"윤서준\" }", JsonCompareMode.LENIENT))
+                .andExpect(jsonPath("$.id").isNumber())
                 .andReturn();
 
         // parse response body as MemberResponse object
