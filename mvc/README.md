284e57c212f15ba8b1414b00bdf1239cffab0c20
spiderman <spiderman@xhift.co.kr>
2025-09-13 15:51:55 +0900

    added MVC project and updated SpringBoot 3.5.5

---
+++ mvc/README.md
@@ -0,0 +1,5 @@
+# Spring MVC + Thymeleaf
+- /member/add
+- /member/edit?id=1
+- /member/list
+- /member/delete?id=1
---
+++ mvc/src/main/java/com/example/demo/controller/MemberController.java
@@ -0,0 +1,53 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Member;
+import com.example.demo.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MemberController {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @GetMapping("/member/add")
+    public String getMemberAdd() {
+        return "member-add";
+    }
+
+    @PostMapping("/member/add")
+    public String postMemberAdd(Member member) {
+        memberRepository.save(member);
+        return "redirect:/member/list";
+    }
+
+    @GetMapping("/member/list")
+    public String getMemberList(Model model) {
+        var members = memberRepository.findAll();
+        model.addAttribute("members", members);
+        return "member-list";
+    }
+
+    @GetMapping("/member/edit")
+    public String getMemberEdit(@RequestParam("id") Long id, Model model) {
+        var member = memberRepository.findById(id).orElseThrow();
+        model.addAttribute("member", member);
+        return "member-edit";
+    }
+
+    @PostMapping("/member/edit")
+    public String postMemberEdit(Member member) {
+        memberRepository.save(member);
+        return "redirect:/member/list";
+    }
+
+    @GetMapping("/member/delete")
+    public String getMemberEdit(@RequestParam("id") Long id) {
+        memberRepository.deleteById(id);
+        return "redirect:/member/list";
+    }
+}
