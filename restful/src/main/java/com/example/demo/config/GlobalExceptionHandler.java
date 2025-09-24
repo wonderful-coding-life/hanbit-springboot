e07597b12c9c76c37101128b3891caeb18db3636
HeesunKim <wonderful.coding.life@gmail.com>
2025-09-24 12:59:33 +0900

    added GlobalExceptionHandler at restful project

---
+++ restful/src/main/java/com/example/demo/config/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.example.demo.config;
+
+import com.example.demo.exception.ExceptionDetails;
+import com.example.demo.exception.MemberNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.sql.SQLException;
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<ExceptionDetails> handleException(SQLException ex) {
+        return ResponseEntity.status(500).body(ExceptionDetails.builder()
+                .timestamp(new Date())
+                .status(500)
+                .reason("데이터베이스에 문제가 발생했습니다.").build());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionDetails> handleException(Exception ex) {
+        return ResponseEntity.status(500).body(ExceptionDetails.builder()
+                .timestamp(new Date())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .reason("서버에 문제가 발생했습니다.").build());
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<ExceptionDetails> handleNotFoundException(MemberNotFoundException ex) {
+        return ResponseEntity.status(404).body(ExceptionDetails.builder()
+                .timestamp(new Date())
+                .status(404)
+                .reason("해당 아이디의 회원이 없습니다.").build());
+    }
+}
---
+++ restful/src/main/java/com/example/demo/exception/ExceptionDetails.java
@@ -0,0 +1,18 @@
+package com.example.demo.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExceptionDetails {
+    private Date timestamp;
+    private Integer status;
+    private String reason;
+}
