a3b4ee8777644005abeb93a42c197170e6c65afc
spiderman <spiderman@xhift.co.kr>
2025-09-25 21:38:33 +0900

    added StructuredOutputConverterTests at openai-tool

---
+++ openai-tool/src/main/java/com/example/demo/model/ActorMovies.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ActorMovies {
+    private String actor;
+    private List<Movie> movies;
+}
