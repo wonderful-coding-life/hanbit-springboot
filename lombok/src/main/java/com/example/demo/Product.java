fe046c0b4ad947699643a7d60e1a31bf7b7d573a
Heesun Kim <wonderful.coding.life@gmail.com>
2025-10-10 10:11:31 +0900

    added lombok project

---
+++ lombok/src/main/java/com/example/demo/Product.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import lombok.*;
+
+//@Getter
+//@Setter
+//@ToString
+//@EqualsAndHashCode
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//@RequiredArgsConstructor
+@Builder
+@EqualsAndHashCode(of = {"name", "price"})
+public class Product {
+    private String name;
+    private String description;
+    private int price;
+}
