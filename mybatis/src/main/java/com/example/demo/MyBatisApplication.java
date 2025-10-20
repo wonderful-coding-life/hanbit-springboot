01b4962c5bf92637df8aba3ba07669381af507d6
spiderman <spiderman@xhift.co.kr>
2025-10-21 08:41:46 +0900

    updated mybatis

--- mybatis/src/main/java/com/example/demo/MyBatisApplication.java
+++ mybatis/src/main/java/com/example/demo/MyBatisApplication.java
@@ -6,10 +6,13 @@ import com.example.demo.model.Article;
 import com.example.demo.model.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -20,6 +23,12 @@ public class MyBatisApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+
+        // 최근의 JDBC 드라이버는 디폴트로 allowMultiQueries가 false로 설정되어 있다.
+        //spring.datasource.url=jdbc:mysql://localhost:3306/mydb?allowMultiQueries=true
+        //List<Member> members = memberMapper.selectAllOrderBy("id; DROP TABLE member; --", "desc");
+        //log.info("Member count: {}", members.size());
+
         int count = memberMapper.selectAllCount();
         log.info("Member count: {}", count);
 
