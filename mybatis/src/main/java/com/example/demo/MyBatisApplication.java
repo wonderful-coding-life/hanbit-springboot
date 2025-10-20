53532a89867261d710ab89b35393dc95d231f317
spiderman <spiderman@xhift.co.kr>
2025-10-21 06:42:28 +0900

    updated mybatis

--- mybatis/src/main/java/com/example/demo/MyBatisApplication.java
+++ mybatis/src/main/java/com/example/demo/MyBatisApplication.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
