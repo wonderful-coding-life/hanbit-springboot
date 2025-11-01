6f923825c7e98fb966866b824cadae1ead31c585
spiderman <spiderman@xhift.co.kr>
2025-11-02 03:34:22 +0900

    added CliApplication at aop

---
+++ aop/src/main/java/com/example/demo/CliApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CliApplication implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // 일반형 인자 file1.txt file2.txt
+        // java -jar app.jar --name=hong --age=20 file1.txt file2.txt
+        List<String> nonOptionArgs = args.getNonOptionArgs();
+        for (String nonOptionArg : nonOptionArgs) {
+            System.out.println("non option arg = " + nonOptionArg);
+        }
+        // 옵션형 인자 --name, --age
+        // java -jar app.jar --name=hong --age=20 file1.txt file2.txt
+        List<String> optionValues = args.getOptionValues("name");
+        for (String optionValue : optionValues) {
+            System.out.println("option arg(name) = " + optionValue);
+        }
+    }
+}
