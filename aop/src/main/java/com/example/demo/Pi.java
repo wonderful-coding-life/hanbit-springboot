56414a66d76012abebec6023ceefca0b0600196c
spiderman <spiderman@xhift.co.kr>
2025-11-02 02:41:04 +0900

    added @Async at aop project

--- aop/src/main/java/com/example/demo/Pi.java
+++ aop/src/main/java/com/example/demo/Pi.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.CompletableFuture;
+
 @Component
+@EnableAsync
 public class Pi {
     @PrintExecutionTime
     double calculate(int points) {
@@ -15,5 +20,18 @@ public class Pi {
             }
         }
         return 4.0 * circle / points;
+    }
+
+    @Async
+    CompletableFuture<Double> calculateAsync(int points) {
+        int circle = 0;
+        for (long i = 0; i < points; i++) {
+            double x = Math.random() * 2 - 1;
+            double y = Math.random() * 2 - 1;
+            if (x * x + y * y <= 1) {
+                circle++;
+            }
+        }
+        return CompletableFuture.completedFuture(4.0 * circle / points);
     }
 }
