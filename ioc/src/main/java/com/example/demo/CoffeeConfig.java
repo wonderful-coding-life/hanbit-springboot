88930078d6d0f76e7ab10fad624da1d3dc9202d6
spiderman <spiderman@xhift.co.kr>
2025-11-16 20:06:44 +0900

    added anonymous class -> lambda expression during bean registration

--- ioc/src/main/java/com/example/demo/CoffeeConfig.java
+++ ioc/src/main/java/com/example/demo/CoffeeConfig.java
@@ -14,4 +14,16 @@ public class CoffeeConfig {
     public CoffeeMachine dripCoffeeMachineBean() {
         return new DripCoffeeMachine();
     }
+
+    @Bean
+    public CoffeeMachine mochaCoffeeMachineBean() {
+//        return new CoffeeMachine() {
+//            @Override
+//            public String brew() {
+//                return "brewing coffee with Mocha Coffee Machine";
+//            }
+//        };
+
+        return () -> "brewing coffee with Mocha Coffee Machine";
+    }
 }
