6105cd5fcf1afb4ff477af11045daba4f991009a
spiderman <spiderman@xhift.co.kr>
2025-12-09 07:40:40 +0900

    updated mvc

--- mvc/src/main/java/com/example/demo/dto/ArticleDto.java
+++ mvc/src/main/java/com/example/demo/dto/ArticleDto.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -18,6 +18,6 @@ public class ArticleDto {
     private String email;
     private String title;
     private String description;
-    private Date created;
-    private Date updated;
+    private LocalDateTime created;
+    private LocalDateTime updated;
 }
